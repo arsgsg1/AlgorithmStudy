98dd4d5ecf21906626f659fa0109913ed4c34634
Garo-o <78212696+Garo-o@users.noreply.github.com>
2021-07-28 23:40:29 +0900

    [ADD]210728

--- 210728/별찍기11.cpp
+++ 210728/별찍기11.cpp
@@ -8,28 +8,23 @@ char star[3][5]={
 };
 char map[MAX][MAX*2-1];
 int N;
-void divide(int r, int c, int n, int k){
-  if(k==0){
+void divide(int r, int c, int n){
+  if(n==3){
     for(int i=0; i<3; i++)for(int j=0; j<5; j++)
       map[r+i][c+j]=star[i][j];
     return;
   }
-  divide(r,c+n,n/2,k-1);
-  divide(r+n,c,n/2,k-1);
-  divide(r+n,c+2*n,n/2,k-1);
+  divide(r,c+n/2,n/2);
+  divide(r+n/2,c,n/2);
+  divide(r+n/2,c+n,n/2);
 }
 
 int main(void){
   cin.tie(0);
   ios_base::sync_with_stdio(0);
   cin>>N;
-  int cnt=-1, temp=N;
-  while(temp%3==0){
-    temp/=2;
-    cnt++;
-  }
   for(int i=0; i<N; i++)for(int j=0; j<N*2-1; j++)map[i][j]=' ';
-  divide(0,0,N/2,cnt);
+  divide(0,0,N);
   for(int i=0; i<N; i++){
     for(int j=0; j<N*2-1; j++)cout<<map[i][j];
     cout<<'\n';
