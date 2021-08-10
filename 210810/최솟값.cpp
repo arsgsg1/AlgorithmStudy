af37a8dd89a7b06f13a93780b31b2346df9e58ab
Garo-o <78212696+Garo-o@users.noreply.github.com>
2021-08-11 03:59:24 +0900

    [ADD]210810

--- 210810/최솟값.cpp
+++ 210810/최솟값.cpp
@@ -4,29 +4,24 @@
 using namespace std;
 typedef unsigned int ui;
 const int MAX=100001, K=19;
-ui arr[MAX],val[MAX][K];
+ui val[MAX][K];
+int Log[MAX];
 
 int main(void){
   int N,M;
   scanf("%d %d",&N,&M);
   memset(val,-1,sizeof(val));
-  for(int i=0; i<N; i++)scanf("%d",arr+i);
+  for(int i=0; i<N; i++)scanf("%d",&val[i][0]);
 
-  for(int i=0; i<N; i++)val[i][0]=min(arr[i],arr[i+1]);
   for(int j=0; j<K; j++)
-    for(int i=0; i<N; i++)
-        if(i+(1<<j)<N)val[i][j+1]=min(val[i][j],val[i+(1<<j)][j]);
+    for(int i=0; i+(1<<j)<N; i++)
+        val[i][j+1]=min(val[i][j],val[i+(1<<j)][j]);
+  for(int i=2; i<=N; i++)Log[i]=Log[i/2]+1;
+
   while(M--){
     int a,b; scanf("%d %d",&a,&b); a--;b--;
     if(a>b)swap(a,b);
-    int n=b-a;
-    ui ans=arr[a];
-    for(int j=K-1; j>=0; j--){
-      if(n&(1<<j)){
-        ans=min(ans,val[a][j]);
-        a+=(1<<j);
-      }
-    }
-    printf("%d\n",ans);
+    int n=Log[b-a+1];
+    printf("%d\n",min(val[a][n],val[b-(1<<n)+1][n]));
   }    
 }
