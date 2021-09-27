f8f6c1453cd0efb7f3dd38c51192ebe9312104b8
arsgsg1 <arsgsg1@gmail.com>
2021-09-27 23:10:53 +0900

    [ADD] 210927

---
+++ 210927/최소직사각형.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+	int answer = 0;
+	int min_w = sizes[0][0], min_h = sizes[0][1];
+	for (int i = 1; i < sizes.size(); i++) {
+		int a1, a2;
+		a1 = max(min_w, sizes[i][0]) * max(min_h, sizes[i][1]);
+		a2 = max(min_w, sizes[i][1]) * max(min_h, sizes[i][0]);
+		if (a1 < a2) {
+			min_w = max(min_w, sizes[i][0]);
+			min_h = max(min_h, sizes[i][1]);
+		}
+		else {
+			min_w = max(min_w, sizes[i][1]);
+			min_h = max(min_h, sizes[i][0]);
+		}
+	}
+	answer = min_w * min_h;
+	return answer;
+}
